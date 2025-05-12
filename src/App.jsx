7b3b6d3f4479d174eb3155bda730830a6dd1d7cb
Ali <alijaberoha@hotmail.com>
2025-05-12 11:45:22 +0200

    start Project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {} from '@fortawesome/free-solid-svg-icons';  // on importe chaque icone qu'on veut utiliser
+
+function App() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
