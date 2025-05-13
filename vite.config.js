abe0bb3896471d9467c3b0340cbbad03d577798b
Ali <alijaberoha@hotmail.com>
2025-05-13 09:55:12 +0200

    deploy4

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ✅ IMPORTANT pour GitHub Pages :
 export default defineConfig({
+  base: '/boxe/', // ← ajoute cette ligne
   plugins: [react()],
-  base:"/boxe"
 })
